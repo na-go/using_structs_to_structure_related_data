1da5edbfed6fc80f7aa9a0994adbbb7436b30712
na-go <kn.mbk201.mynk@gmail.com>
2021-07-09 23:00:51 +0900

    5.1 addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn defining_and_instantiating_structs () {
+
+
+    let mut user1 = User {
+        username: String::from("na-go"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+        active: true 
+    };
+
+    println!("user1's username:{}", user1.username);
+    println!("user1's email:{}", user1.email);
+    println!("user1's sigm in count:{}", user1.sign_in_count);
+    println!("user1's active:{}", user1.active);
+    
+    user1.sign_in_count = 3;
+    println!("user1's sigm in count:{}", user1.sign_in_count);
+
+    let user2 = build_user(String::from("examlpleeeee@example.com"), String::from("whoami"));
+    println!("user2's username:{}", user2.username);
+}
+
 fn main() {
-    println!("Hello, world!");
+    defining_and_instantiating_structs();
+    
 }
